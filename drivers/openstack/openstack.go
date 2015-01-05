8f3502df0dbcca1d20df2e643a4e84b9987b8b5d
Ash Wilson <ash.wilson@rackspace.com>
2015-01-05 11:34:59 -0500

    Shelve docker install output in /var/log.
    Signed-off-by: Ash Wilson <ash.wilson@rackspace.com>

--- drivers/openstack/openstack.go
+++ drivers/openstack/openstack.go
@@ -703,7 +703,7 @@ func (d *Driver) installDocker() error {
 
 	if err := d.sshExec([]string{
 		`apt-get install -y curl`,
-		`curl -sSL https://get.docker.com | /bin/sh`,
+		`curl -sSL https://get.docker.com | /bin/sh >/var/log/docker-install.log 2>&1`,
 		`service docker stop`,
 		`curl -sSL https://ehazlett.s3.amazonaws.com/public/docker/linux/docker-1.4.1-136b351e-identity -o /usr/bin/docker`,
 		`echo "export DOCKER_OPTS=\"--auth=identity --host=tcp://0.0.0.0:2376\"" >> /etc/default/docker`,
